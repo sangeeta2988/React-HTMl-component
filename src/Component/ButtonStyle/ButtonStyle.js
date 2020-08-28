94167fed2b106086663fa5a933db092a0a59f9f9
sangeeta2988 <agrawal.sangeeta2988@gmail.com>
2020-08-28 14:12:23 +0530

    finish till large button

--- src/Component/ButtonStyle/ButtonStyle.js
+++ src/Component/ButtonStyle/ButtonStyle.js
@@ -1,73 +1,194 @@
-import React from 'react';
-import './ButtonStyle.css';
-import WhiteIcon from '../../assets/images/plus_icon.png';
-import BlackIcon from '../../assets/images/plus_black.png';
+import React from "react";
+import "./ButtonStyle.css";
+import WhiteIcon from "../../assets/images/plus_icon.png";
+import BlackIcon from "../../assets/images/plus_black.png";
+import plus_giant from "../../assets/images/plus_giant.png";
+import RightArrow from "../../assets/images/Path 4@2x.png";
+import Plush from "../../assets/images/plus.png";
+// Path 4@2x
+
 export const AddDefaultButton = () => {
-    return(<>
-    <div className="DefaultButton">
-    <div class="button-name">Default</div>
-            <div class="Button">
-                <img src={WhiteIcon} alt="" srcset="" />
-            </div>
-    </div>
-    </>)
-}
+  return (
+    <>
+      <div className="DefaultButton">
+        <div class="button-name">Default</div>
+        <div class="Button">
+          <img src={WhiteIcon} alt="" srcset="" />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export const AddHoverButton = () => {
-    return(<>
-    <div className="DefaultButton">
-    <div class="button-name">Hover</div>
-            <div class="Button HoverEvent">
-                <img src={BlackIcon} alt="" srcset="" />
-            </div>
-    </div>
-    </>)
-}
+  return (
+    <>
+      <div className="DefaultButton">
+        <div class="button-name">Hover</div>
+        <div class="Button HoverEvent">
+          <img src={BlackIcon} alt="" srcset="" />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export const AddHitButton = () => {
-    return(<>
-    <div className="DefaultButton">
-    <div class="button-name">Hit</div>
-            <div class="Button hitEvent">
-                <img src={BlackIcon} alt="" srcset="" />
-            </div>
-    </div>
-    </>)
-}
-
+  return (
+    <>
+      <div className="DefaultButton">
+        <div class="button-name">Hit</div>
+        <div class="Button hitEvent">
+          <img src={BlackIcon} alt="" srcset="" />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export const DefaultButton = () => {
-    return(<>
-    <div class="DefaultButton">
+  return (
+    <>
+      <div class="DefaultButton">
         <div class="button-name">Default</div>
         <div class="FAB-button fab-default">save</div>
-    </div>
-    </>)
-}
+      </div>
+    </>
+  );
+};
 
 export const HoverButton = () => {
-    return(<>
-    <div class="DefaultButton">
+  return (
+    <>
+      <div class="DefaultButton">
         <div class="button-name">Hover</div>
         <div class="FAB-button fab-hover">save</div>
-    </div>
-    </>)
-}
+      </div>
+    </>
+  );
+};
 
 export const HitButton = () => {
-    return(<>
-    <div class="DefaultButton">
+  return (
+    <>
+      <div class="DefaultButton">
         <div class="button-name">Hit</div>
         <div class="FAB-button fab-hit">save</div>
-    </div>
-    </>)
-}
+      </div>
+    </>
+  );
+};
 
 export const DisabledButton = () => {
-    return(<>
-    <div class="DefaultButton">
+  return (
+    <>
+      <div class="DefaultButton">
         <div class="button-name">Disabled</div>
         <div class="FAB-button fab-disable">save</div>
+      </div>
+    </>
+  );
+};
+
+export const GiantAddbtn = (props) => {
+  return (
+    <div class="DefaultButton">
+      <div class="button-name">Default</div>
+      <div class="giant-default">
+        <img src={plus_giant} alt="plus_giant" srcset="" />
+      </div>
     </div>
-    </>)
-}
+  );
+};
+
+export const GiantAddHoverBtn = (props) => {
+  return (
+    <div class="DefaultButton">
+      <div class="button-name">Hover</div>
+      <div class="giant-hover">
+        <img src={plus_giant} alt="plus_giant" srcset="" />
+      </div>
+    </div>
+  );
+};
+
+// Text buttons
+
+export const TextBtnDefault = (props) => {
+  return (
+    <div class="DefaultButton">
+      <div class="button-name">Hover</div>
+      <p class="text-button-default">
+        Next
+        <img src={RightArrow} alt="" srcset="" />
+      </p>
+    </div>
+  );
+};
+
+export const TextBtnHover = (props) => {
+  return (
+    <div class="DefaultButton">
+      <div class="button-name">Hover</div>
+      <p class="text-button-hover">
+        Next
+        <img src={RightArrow} alt="" srcset="" />
+      </p>
+    </div>
+  );
+};
+
+export const AddTextBtn = (props) => {
+  return (   
+    <div class="button-box">
+      <p class="add-text-button">
+        <img src={Plush} alt="" srcset="" />
+        Add variation…
+      </p>
+    </div>
+  );
+};
+
+export const Link = (props) => {
+    return (
+        <a href="">I am a text link</a>
+    );
+  };
+
+//   LARGE BUTTON
+
+  export const LargePrimaryDefaultBtn = (props) => {
+    return (
+        <div class="button-box">
+        <div class="button-name">Default</div>
+        <div class="primary-l">Create Style</div>
+      </div>
+    );
+  };
+
+  export const LargePrimaryHoverBtn = (props) => {
+    return (
+        <div class="button-box">
+        <div class="button-name">Hover</div>
+        <div class="primary-l primary-l-hover">Create Style</div>
+      </div>
+    );
+  };
+
+  export const LargeSecondaryDefaultBtn = (props) => {
+    return (
+        <div class="button-box">
+        <div class="button-name">Default</div>
+        <div class="Button cancel-default">Cancel</div>
+      </div>
+    );
+  };
+
+  export const LargeSecondaryHoverBtn = (props) => {
+    return (
+        <div class="button-box">
+        <div class="button-name">Hover</div>
+        <div class="Button cancel-hover">Cancel</div>
+      </div>
+    );
+  };
--- src/Screen/ButtonsStyle.js
+++ src/Screen/ButtonsStyle.js
@@ -1,81 +1,75 @@
-import React, { Component } from 'react';
-import { TitleText } from '../Component/FontStyle/Heading/Heading';
-import { AddDefaultButton , AddHoverButton , AddHitButton , DefaultButton , HoverButton ,
-    HitButton , DisabledButton } from '../Component/ButtonStyle/ButtonStyle';
-class ButtonsStyle extends Component{
-    render()
-    {
-        return(<>
+import React, { Component } from "react";
+import { TitleText } from "../Component/FontStyle/Heading/Heading";
+import {
+  AddDefaultButton,
+  AddHoverButton,
+  AddHitButton,
+  DefaultButton,
+  HoverButton,
+  HitButton,
+  DisabledButton,
+  GiantAddbtn,
+  GiantAddHoverBtn,
+  TextBtnDefault,
+  TextBtnHover,
+  AddTextBtn,
+  Link,
+  LargePrimaryDefaultBtn,
+  LargePrimaryHoverBtn,
+  LargeSecondaryDefaultBtn,
+  LargeSecondaryHoverBtn,
+} from "../Component/ButtonStyle/ButtonStyle";
+class ButtonsStyle extends Component {
+  render() {
+    return (
+      <>
         <div class="container">
-            <TitleText>SC Buttons</TitleText>
-        <div class="content">
+          <TitleText>SC Buttons</TitleText>
+          <div class="content">
             <div class="partial">
-                {/* <p class="button-heading">Fixed Action Button (FAB)</p> */}
-                <p class="button-title">Universal add button</p>
+              {/* <p class="button-heading">Fixed Action Button (FAB)</p> */}
+              <p class="button-title">Universal add button</p>
+              <div>
+                <AddDefaultButton />
+                <AddHoverButton />
+                <AddHitButton />
+              </div>
+
+              <p class="button-title">Text FAB</p>
+              <div>
+                <DefaultButton />
+                <HoverButton />
+                <HitButton />
+                <DisabledButton />
+              </div>
+              <div class="text-button2">
+                <p class="button-title">Giant add button</p>
+
+                <GiantAddbtn />
+                <GiantAddHoverBtn />
+              </div>
+
+              <div class="text-button">
                 <div>
-                    <AddDefaultButton />
-                    <AddHoverButton />
-                    <AddHitButton />
+                  <p class="button-heading">Text buttons</p>
+                  <div class="left">
+                    <p class="button-title">Text + icon</p>
+                    <TextBtnDefault />
+                    <TextBtnHover />
+                  </div>
+                  <div class="right">
+                    <p class="button-title">Add text button</p>
+                    <AddTextBtn />
+                  </div>
                 </div>
-
-                <p class="button-title">Text FAB</p>
-                <div>
-                    <DefaultButton />
-                    <HoverButton />
-                    <HitButton />
-                    <DisabledButton />
-                </div>
-                {/* <div class="text-button2">
-                    <p class="button-heading">Giant add button</p>
-                    
-                    <div class="button-box">
-                        <p class="button-name">Default</p>
-                        <div class="giant-default"><img src="./../icon/plus_giant.png" alt="" srcset="" /></div>
-                        
-                    </div>
-                    <div class="button-box">
-                        <p class="button-name">Hover</p>
-                        <div class="giant-hover"><img src="./../icon/plus_giant.png" alt="" srcset="" /></div>
-                    </div>
-                </div> */}
-
-                {/* <div class="text-button">
-                    <div>
-                        <p class="button-heading">Text buttons</p>
-                        <div class="left">
-                            <p class="button-title">Text + icon</p>
-
-                            <div class="button-box">
-                                <p class="button-name">Default</p>
-                                <p class="text-button-default">Next<img src="./../icon/Path 4@2x.png" alt="" srcset="" /></p>
-                            </div>
-                            <div class="button-box">
-                                <p class="button-name">Hover</p>
-                                <p class="text-button-hover">Next<img src="./../icon/Path 4@2x.png" alt="" srcset="" /></p>
-                            </div>
-                        </div>
-
-
-
-                        <div class="right">
-                            <p class="button-title">Add text button</p>
-
-                            <div class="button-box">
-                                <p class="add-text-button"><img src="./../icon/plus.png" alt="" srcset="" />Add variation…</p>
-                            </div>
-
-                        </div>
-                    </div>
-                </div> */}
-               
-                
+              </div>
+              <div>
+                <p class="button-title">Links</p>
+                <Link />
+              </div>
             </div>
-            
-      
 
-
-
-    {/* <div class="partial">
+            <div class="partial">
 
 
         <p class="button-heading">Large buttons</p>
@@ -83,26 +77,14 @@ class ButtonsStyle extends Component{
         <div>
             <div class="half1">
                 <p class="button-title">Primary</p>
-                <div class="button-box">
-                    <div class="button-name">Default</div>
-                    <div class="primary-l">Create Style</div>
-                </div>
-                <div class="button-box">
-                    <div class="button-name">Hover</div>
-                    <div class="primary-l primary-l-hover">Create Style</div>
-                </div>
+                <LargePrimaryDefaultBtn />
+                <LargePrimaryHoverBtn />               
             </div>
 
             <div class="half2">
-                <p class="button-title">Secondary</p>
-                <div class="button-box">
-                    <div class="button-name">Default</div>
-                    <div class="button cancel-default">Cancel</div>
-                </div>
-                <div class="button-box">
-                    <div class="button-name">Hover</div>
-                    <div class="button cancel-hover">Cancel</div>
-                </div>
+                <p class="button-title">Secondary</p>               
+                <LargeSecondaryDefaultBtn />                
+                <LargeSecondaryHoverBtn />
             </div>
         </div>
 
@@ -156,12 +138,12 @@ class ButtonsStyle extends Component{
             </div>
         </div>
 
-    </div> */}
-
     </div>
-    </div>
-        </>)
-    }
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default ButtonsStyle;
